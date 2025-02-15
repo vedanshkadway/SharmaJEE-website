0e6011b0f6de186dc776fe6cc491e8027b9a2478
vedansh kadway <vedanshkadway051@gmail.com>
2025-02-15 14:30:33 +0530

    aboutUS Error fixed

--- src/app/aboutus/page.tsx
+++ src/app/aboutus/page.tsx
@@ -1,11 +1,9 @@
 import { TimelineDemo } from '@/components/Timeline'
 import React from 'react'
-import Intro from './Intro'
 
 function aboutus() {
   return (
     <div>
-      <Intro/>
         <TimelineDemo/>
       
     </div>
